6de3f721424daa9dd5023fd47ab612062266bce4
Benjamin Chan <bchan@e1z2r5p43.42.us.org>
2018-02-23 18:29:09 -0800

    asdf

--- printf/modify_double.c
+++ printf/modify_double.c
@@ -6,7 +6,7 @@
 /*   By: bchan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 13:13:15 by bchan             #+#    #+#             */
-/*   Updated: 2018/02/21 13:50:09 by bchan            ###   ########.fr       */
+/*   Updated: 2018/02/23 18:25:23 by bchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	truncate_dec(char *result, int n)
 	i = 0;
 	while (result[i] && result[i] != '.')
 		i++;
-	while (n-- > 0)
+	while (n-- >= 0)
 		i++;
 	result[i] = '\0';
 }
@@ -33,7 +33,7 @@ void	swap(char *result, int i, int j)
 	result[j] = c;
 }
 
-void	add_exp(char *result, int count)
+char	*add_exp(char *result, int count)
 {
 	char *temp;
 
@@ -53,10 +53,11 @@ void	add_exp(char *result, int count)
 		temp = ft_itoa(count * -1);
 		ft_strpstr(&result, temp);
 	}
-	ft_strdel(temp);
+	ft_strdel(&temp);
+	return (result);
 }
 
-char	*sci_convert(char *result)
+char	*sci_convert(char *result, int n)
 {
 	int i;
 	int count;
@@ -65,7 +66,7 @@ char	*sci_convert(char *result)
 	count = 0;
 	while (result[i] && result[i] != '.')
 		i++;
-	while (ft_atoi(result) > 10 || ft_atoi(result) < -10)
+	while (ft_atoi(result) >= 10 || ft_atoi(result) <= -10)
 	{
 		swap(result, i, i - 1);
 		count++;
@@ -74,28 +75,37 @@ char	*sci_convert(char *result)
 	while (ft_atoi(result) < 1 && ft_atoi(result) > -1 && result[i])
 	{
 		swap(result, i, i + 1);
-		ft_memmove(&result[i - 1], result[i], ft_strlen(&result[i]) + 1);
+		ft_memmove(&result[i - 1], &result[i], ft_strlen(&result[i]) + 1);
 		count--;
 	}
-	truncate_dec(result, 6);
+	truncate_dec(result, n);
 	ft_strpchar(&result, 'e');
-	add_exp(result, count);
+	result = add_exp(result, count);
 	return (result);
 }
 
 char	*modify_double(t_print *form, char *result)
 {
-	int i;
+	int		i;
+	char	*temp;
 
+	if (form->precision == -1)
+		i = 6;
+	else
+		i = form->precision;
 	if (form->spec == 'f' || form->spec == 'F')
-		truncate_dec(result, 6);
+		truncate_dec(result, i);
 	else if (form->spec == 'e' || form->spec == 'E')
-		result = sci_convert(result);
-	if (form->spec == 'F' || form->spec == 'E')
+		result = sci_convert(result, i);
+	else if (form->spec == 'g' || form->spec == 'G')
+		result = find_shortest(form, result);
+	if (form->spec == 'F' || form->spec == 'E' || form->spec == 'G')
+		ft_capitalize(result);
+	if (!(ft_strchr(form->flags, '#')) && form->precision == 0 && form->spec
+			!= 'g' && form->spec != 'G')
 	{
-		i = 0;
-		while (result[i])
-			ft_toupper(result[i++]);
+		temp = ft_strchr(result, '.');
+		ft_memmove(temp, temp + 1, ft_strlen(temp));
 	}
 	return (result);
 }
